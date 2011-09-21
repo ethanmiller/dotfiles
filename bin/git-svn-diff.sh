4af5764207bcae03e0fa5ebfdae09afabf496a65
Ethan <emiller@energy-solution.com>
2011-09-20 21:01:51 -0700

    adds git svn diff

--- bin/git-svn-diff.sh
+++ bin/git-svn-diff.sh
@@ -2,7 +2,6 @@
 #
 # git-svn-diff originally by (http://mojodna.net/2009/02/24/my-work-git-workflow.html)
 # modified by mike@mikepearce.net
-# modified by aconway@[redacted] - handle diffs that introduce new files
 #
 # Generate an SVN-compatible diff against the tip of the tracking branch
 
@@ -14,16 +13,14 @@ TRACKING_BRANCH=`git svn info | grep URL | sed -e 's/.*\/branches\///'`
 if [[ "$TRACKING_BRANCH" =~ URL.* ]]
 then
         TRACKING_BRANCH=`git config --get svn-remote.svn.fetch | sed -e 's/.*:refs\/remotes\///'`
-fi
+	fi
 
-# Get the highest revision number
-REV=`git svn find-rev $(git rev-list --date-order --max-count=1 $TRACKING_BRANCH)`
+	# Get the highest revision number
+	REV=`git svn find-rev $(git rev-list --date-order --max-count=1 $TRACKING_BRANCH)`
 
-# Then do the diff from the highest revision on the current branch 
-# and masssage into SVN format
-git diff --no-prefix $(git rev-list --date-order --max-count=1 $TRACKING_BRANCH) $* |
-sed -e "/--- \/dev\/null/{ N; s|^--- /dev/null\n+++ \(.*\)|---\1	(revision 0)\n+++\1	(revision 0)|;}" \ 
-    -e "s/^--- .*/&	(revision $REV)/" \ 
-    -e "s/^+++ .*/&	(working copy)/" \ 
-    -e "s/^diff --git [^[:space:]]*/Index:/" \ 
-    -e "s/^index.*/===================================================================/"
+	# Then do the diff from the highest revition on the current branch
+	git diff --no-prefix $(git rev-list --date-order --max-count=1 $TRACKING_BRANCH) $* |
+	sed -e "s/^+++ .*/&     (working copy)/" -e "s/^--- .*/&        (revision $REV)/" \
+	-e "s/^diff --git [^[:space:]]*/Index:/" \
+	-e "s/^index.*/===================================================================/"
+
